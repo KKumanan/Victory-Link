bafcfcfa35bbea70540fc95f0dd2836ef9833aa4
KKumanan <krishna.kumanan@gmail.com>
2024-09-03 18:27:54 -0400

    Cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to MERN Stack Project</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Authentication</h1>
+        <Switch>
+          <Route exact path="/" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
